5215531f6d6d12ea29f1274d79da0e15818a19ad
kristinasabaroedin <kristinasabaroedin@gmail.com>
2020-06-18 16:47:45 +1000

    cleaned up a bit of getkivalues_02.m

--- getkivalues_02.m
+++ getkivalues_02.m
@@ -1,4 +1,5 @@
-% script to calculate Ki values for one subject
+function [] = getkivalues_02(projdir, subject)
+% function to calculate Ki values for one subject
 
 % This script is divided into several parts:
 % 1 - loading striatal and cerebelum masks, using the masks to get time
@@ -16,23 +17,22 @@
 % 6 - Clearance rate in striatal region is estimated by dividing the TACS
 % in each region by the cerebellum BIF
 
-projdir = '/scratch/kg98/kristina/Projects/MMH002/';
-subject='sub-012';
-framesdir = [projdir,'datadir/PET_files/',subject,'/t1-space/'];
-
 Mode = {'voxelwise','regionwise'};
 WhichMode = Mode{2};
-
-cd(framesdir)
-
-frames_files=dir(framesdir);
-frames_files(1:2) = [];
 
 % create duration of frames; there are 26 frames
 dur = [.5 1 1 1 1 2 2 2 3 3 3 5 * ones(1,15)];
 
 frames = [1:26];
 frames = reshape(frames,26,1);
+
+framesdir = [projdir,'datadir/PET_files/',subject,'/t1-space/'];
+
+cd(framesdir)
+
+frames_files=dir(framesdir);
+frames_files(1:2) = [];
+
 
 roidir = [projdir,'datadir/derivatives/fmriprep_resample2mni_2mm/fmriprep/',subject,'/rois/'];
 
@@ -56,10 +56,17 @@ str_sens_mask = niftiread([roidir,'Lk31_space-T1.nii']);
 str_sens_mask_index = find(str_sens_mask == 1);
 % str_sens = reshape(str_sens,[],length(str_sens));
 
+% whole striatum
+str_whole_mask = niftiread([roidir,'Lk3_space-T1.nii']);
+str_whole_mask_index = find(str_whole_mask == 1);
+% str_sens = reshape(str_sens,[],length(str_sens));
+
 tacs_str_assoc = [];
 tacs_str_vent = [];
 tacs_str_sens = [];
 tacs_cer = [];
+
+tacs_str_whole = [];
 
 for i = 1:26
     frame = niftiread([framesdir,frames_files(i).name]);
@@ -67,12 +74,15 @@ for i = 1:26
     tacs_str_vent(:,i) = frame(str_vent_mask_index);
     tacs_str_sens(:,i) = frame(str_sens_mask_index);
     tacs_cer(:,i) = frame(cer_mask_index);
+    tacs_str_whole(:,i) = frame(str_whole_mask_index);
 end
 
 % Get mean tacs for each striatal region for regionwise analysis
 str_assoc_mean = mean(tacs_str_assoc);
 str_sens_mean = mean(tacs_str_sens);
 str_vent_mean = mean(tacs_str_vent);
+
+str_whole_mean = mean(tacs_str_whole);
 
 % Get cerebellum blood input function
 % bif{ii} = mean(t_tac(xroi{1},:)); What was in the script
@@ -104,6 +114,9 @@ switch WhichMode
         % Ventral striatum
         Rn_vent = bsxfun(@rdivide, double(tacs_str_vent(:,k)),bif(k));
         param_vent = Rn_vent/Bn;
+        % Whole striatum
+        Rn_whole = bsxfun(@rdivide, double(tacs_str_whole(:,k)),bif(k));
+        param_whole = Rn_whole/Bn;
     case 'regionwise'
     % Divide TACS by BIF: division using the mean tac of the region instead of voxel-wise
         % get ki value for associative striatum
@@ -115,17 +128,40 @@ switch WhichMode
         % get ki value for ventral striatum
         Rn_str_vent = bsxfun(@rdivide, double(str_vent_mean(:,k)),bif(k));
         param_str_vent = Rn_str_vent/Bn;
+        % get ki value for whole striatum
+        Rn_str_whole = bsxfun(@rdivide, double(str_whole_mean(:,k)),bif(k));
+        param_str_whole = Rn_str_whole/Bn;
 end       
 % the output for the mode of analysis above is a two column matrois, first
 % column is clearance rate (Kicer) and the second is the volume of
 % distribution
 
-% Plot clearance rate (Ki)
+% Plot clearance rate (Ki) for assoc str
 figure(1)
+subplot(2,2,1)
 scatter(Bn(1,:),Rn_str_assoc)
 xlabel('auc/bif')
 ylabel('tacs/bif')
 title([subject,': assoc str patlak plot; ki value ',sprintf('%.6f',param_str_assoc(1,1))])
+
+subplot(2,2,2)
+scatter(Bn(1,:),Rn_str_vent)
+xlabel('auc/bif')
+ylabel('tacs/bif')
+title([subject,': ventral str patlak plot; ki value ',sprintf('%.6f',param_str_vent(1,1))])
+
+subplot(2,2,3)
+scatter(Bn(1,:),Rn_str_sens)
+xlabel('auc/bif')
+ylabel('tacs/bif')
+title([subject,': sens str patlak plot; ki value ',sprintf('%.6f',param_str_sens(1,1))])
+
+subplot(2,2,4)
+scatter(Bn(1,:),Rn_str_whole)
+xlabel('auc/bif')
+ylabel('tacs/bif')
+title([subject,': whole str patlak plot; ki value ',sprintf('%.6f',param_str_whole(1,1))])
+
 
 % Plot TACs
 figure(2)
@@ -143,11 +179,17 @@ xlabel('frames')
 ylabel('MBq')
 
 cd([projdir,'datadir/PET_files/',subject,'/'])
-% mkdir('patlak')
-% cd('patlak')
-% saveas(figure(1),'assoc_str_ki.fig')
-% saveas(figure(2),'mean_tacs.fig')
-% 
-% close all
-% 
-% save([subject,'_tacs_stri_patlak.mat'])
+mkdir('patlak')
+cd('patlak')
+saveas(figure(1),'str_ki.fig')
+saveas(figure(2),'mean_tacs.fig') 
+ 
+regions = {'assoc','ventral','sens','whole_str','cereb'};
+regions_tacs = {str_assoc_mean, str_vent_mean, str_sens_mean, str_whole_mean, bif};
+regions_ki = {param_str_assoc, param_str_vent, param_str_sens, param_str_whole};
+regions_ki_names = {'assoc','ventral','sens','whole_str'};
+
+save([subject,'_tacs_stri_patlak.mat'],'regions','regions_tacs','regions_ki','regions_ki_names')
+
+close all
+end
