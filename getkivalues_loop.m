399b8f71be382d9314c701c02812d5d1c622d328
kristinasabaroedin <kristinasabaroedin@gmail.com>
2020-06-20 20:00:32 +1000

    Added path to script folder

--- getkivalues_loop.m
+++ getkivalues_loop.m
@@ -1,11 +1,12 @@
 projdir = '/scratch/kg98/kristina/Projects/MMH002/';
+addpath([projdir,'code/'])
 fileID = fopen([projdir,'MMH002_PET_subjects.txt']);
 ParticipantIDs = textscan(fileID,'%s');
 ParticipantIDs = ParticipantIDs{1};
 % compute numsubs
 numSubs = length(ParticipantIDs);
 
-for i = 1:numSubs
+for i = 41:41
     subject = ParticipantIDs{i}
     getkivalues_02(projdir, subject)
 end
